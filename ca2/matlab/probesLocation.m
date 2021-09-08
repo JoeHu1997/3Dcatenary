d548b43456a0cb8a32d4ce2f984f42e2b1fb141c
JoeHu1997 <62299081+JoeHu1997@users.noreply.github.com>
2021-09-08 13:30:06 +0800

    testwavewithsponge

--- ca2/matlab/probesLocation.m
+++
@@ -1,8 +0,0 @@
-cd ([root '/system'])
-fid     =   ['pointLocation' num2str(cable_no) '.txt'];
-fileID  =   fopen(fid,'w+');
-fprintf(fileID,'( %.12f %.12f %.12f )\n',X_tmp');
-
-fclose(fileID);
-
-
--- ca2/matlab/solNewmark.m
+++
@@ -1,4 +0,0 @@
-function    [A  V]  =   solNewmark(a0,a2,a3,a6,a7,tA,tV,deltaU)
-    A = a0*deltaU    -a2*tV  -a3*tA;
-    V = tV           +a6*tA  +a7*A;
-end
